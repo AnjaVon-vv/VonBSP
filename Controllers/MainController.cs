7ff79b98ef7e6ba11d99ece4f4dc122ad75157f4
Von <AQIyou@yeah.net>
2020-09-08 03:16:47 +0800

    添加项目文件。

---
+++ Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VonBSP.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Search()
+        {
+
+
+            return View();
+        }
+     
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+    }
+}
